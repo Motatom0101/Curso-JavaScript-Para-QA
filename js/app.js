20030a77f337c837985362461746cb35d09fa9a3
Tiago Mota <tmota.seg@gmail.com>
2025-08-13 02:17:04 -0300

    função com 'return'

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
-var n1 = 5
-var n2 = 'cinco'
+console.log('Olá, mundo!')
 
-var total = n1 + parseInt(n2)
-console.log(total)
+var usarName = 'Mota'
+
+document.getElementById('user-name').innerHTML = usarName
+
+console.log('================================')
+
+/*
+function soma(n1, n2){
+    console.log(n1 + n2)
+}
+soma(14, 15)
+
+
+function boasVindas(nome){
+   alert(nome + ', Seja bem-vindo(a)!') 
+}
+
+boasVindas('Tiago')
+
+*/
+
+function soma(n1, n2){
+    return n1 + n2
+}
+
+var resultado = soma(7,7)
+console.log(resultado)
